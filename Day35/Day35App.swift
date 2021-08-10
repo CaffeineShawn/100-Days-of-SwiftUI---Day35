a1bcce3dca5f9322f969649aeedbf9dde5e8273f
CaffeineShawn <52215406+CaffeineShawn@users.noreply.github.com>
2021-08-10 20:19:33 +0800

    Initial Commit

---
+++ Day35/Day35App.swift
@@ -0,0 +1,17 @@
+//
+//  Day35App.swift
+//  Day35
+//
+//  Created by Shaw Young on 2021/8/10.
+//
+
+import SwiftUI
+
+@main
+struct Day35App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
